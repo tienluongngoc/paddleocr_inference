5f11288a39b464a3f9a13d64ab14d005bbae88cf
tienluongngoc <ngoctien.hust@gmail.com>
2022-03-24 07:56:23 +0000

    init

---
+++ infer.sh
@@ -0,0 +1,6 @@
+python main.py \
+        --image_dir=img \
+        --rec_model_dir="./model" \
+        --use_gpu=true \
+        --warmup=false \
+        --rec_char_dict_path=model/plate/dict.txt
